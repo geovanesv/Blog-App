95c5c3cb6f6d476feea33adf17fa7d3c67870b25
Geovane Araujo <geovanesv89@gmail.com>
2023-12-30 23:51:42 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,41 +1,10 @@
-
-// const mongoose = require("mongoose")
-
-// const connect = () => {
-//     mongoose.connect(`mongodb+srv://usuarioDB:senhaUser@cluster0.jqyx0.mongodb.net/?retryWrites=true&w=majority`)
-
-//     const connection = mongoose.connection;
-
-//     connection.on("error", () => {
-//         console.error("Erro ao conectar com o mongoDB")
-//     })
-
-//     connection.on("open", () => {
-//         console.log("Conetado ao mongoDB com sucesso!")
-//     })
-// }
-
-// connect();
-
-// module.exports = mongoose;
-
 const mongoose = require("mongoose");
 
-const connect = () => {
-  // mongoose.connect("mongodb+srv://myuser:mypassword@mycluster.us-east-1.mongodb.net/mydb");
-  mongoose.connect("mongodb://localhost:27017/blogapp");
+mongoose.set('strictQuery', false);
 
-  const connection = mongoose.connection;
 
-  connection.on("error", () => {
-    console.error("Erro ao conectar com o mongoDB");
-  })
-
-  connection.on("open", () => {
-    console.log("Conetado ao mongoDB com sucesso!");
-  })
-}
-
-connect();
-
-module.exports = mongoose;
+mongoose.connect("mongodb://127.0.0.1:27017/blogapp").then(()=>{
+    console.log("connected!");
+}).catch((err)=>{
+    console.log(err);
+})
